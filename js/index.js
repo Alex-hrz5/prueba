e4b233aa40672e068dc10739a3d1f5beab696c7f
Alex-hrz5 <111317344+Alex-hrz5@users.noreply.github.com>
2022-08-15 15:38:53 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 Siiuuuu, ahora somos Partner 💞');
+    alert('😱🥰Nuestra boda Is Mañana🥰😱');
+    location.href = 'https://www.youtube.com/watch?v=SmF4wY7U7QE';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 Dame tu name 🥰');
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
